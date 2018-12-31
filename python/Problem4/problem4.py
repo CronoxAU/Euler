f1ade24c9bd473beb42eb5b5d0ee20022946b7c6
Chris McLean <christopher.mclean@gmail.com>
2018-12-31 20:59:17 +0800

    Implimented problem 4 in Python
    implimented solution and testing for problem 4 in python

--- python/Problem4/problem4.py
+++ python/Problem4/problem4.py
@@ -1,6 +1,8 @@
 #Solution to Project Euler problem 4 - https://projecteuler.net/problem=4
 #Find the largest palindrome made from the product of two 3-digit numbers.
+import math
 
+#Checks if the number supplied is a palindrome - This function is no longer used in this solution
 def isPalindrome(num):
   original = str(num)
   reverse = original[::-1]
@@ -8,5 +10,47 @@ def isPalindrome(num):
     return True
   return False
 
-result = 1
-print(result)
+#Returns an integer palindrome using the prefix supplied as the starting digits
+def makePalindrome(prefix):
+  prefixString = str(prefix)
+  suffixString = prefixString[::-1]
+  fullString = prefixString + suffixString
+  return int(fullString)
+
+#returns the first half of the digits of the supplied number
+def getFirstHalf(num):
+  numString = str(num)
+  mid= math.floor((len(numString) + 1) / 2)
+  return int(numString[:mid])
+
+#checks if the supplied num is the product of 2 three digit numbers
+def isProductOfTwo3DigitNumbers(num):
+  upperLimit = math.floor(math.sqrt(num))
+  if upperLimit > 1000 or upperLimit < 100:
+    return False
+  for i in range(upperLimit, 100, -1):
+    if num % i == 0:
+      j = int(num / i)
+      if j >= 100 and j < 1000:
+        if num % j == 0:
+          return True
+  return False
+
+#finds the largest palindromic number, which is a product of two 3 digit numbers, below the supplied upper limit
+#returns 0 if no such number can be found
+def findLargestPalindromeProductOfTwo3DigitNumbers(upperLimit):
+  found = False
+  #split the upper limit in half
+  initialFirstHalf = getFirstHalf(upperLimit)
+  # loop through each possible first half of a number above 100 (ie the first half of 100001 the smallest palindromic product of two 3 digit numbers)
+  #start that the largest numbers first and decrement through, this way the first one found will be the largest.
+  for i in range(initialFirstHalf, 100, -1):
+    #create a palindrome using the first half as a root
+    currentPalindrome = makePalindrome(i)
+    #check if this palindrome is a product two 3 digit numbers. If it is the result has been found.
+    if isProductOfTwo3DigitNumbers(currentPalindrome):
+      return currentPalindrome
+
+  return 0
+
+print(findLargestPalindromeProductOfTwo3DigitNumbers(999*999))
--- python/Problem4/test4.py
+++ python/Problem4/test4.py
@@ -15,6 +15,35 @@ class Test_TestProblem4(unittest.TestCase):
     def test_isPalindrome9010(self):
         self.assertEqual(problem4.isPalindrome(9010), False)
 
+    def test_makePalindrome90(self):
+        self.assertEqual(problem4.makePalindrome(90), 9009)
+
+    def test_makePalindrome989(self):
+        self.assertEqual(problem4.makePalindrome(989), 989989)
+
+    def test_makePalindrome987(self):
+        self.assertEqual(problem4.makePalindrome(987), 987789)
+
+    def test_getFirstHalf987654(self):
+        self.assertEqual(problem4.getFirstHalf(987654), 987)
+
+    def test_getFirstHalf3215(self):
+        self.assertEqual(problem4.getFirstHalf(3215), 32)
+
+    def test_isProductOfTwo3DigitNumbers10(self):
+        self.assertEqual(problem4.isProductOfTwo3DigitNumbers(10), False)
+
+    def test_isProductOfTwo3DigitNumbers100000(self):
+        self.assertEqual(problem4.isProductOfTwo3DigitNumbers(100000), True)
+
+    def test_isProductOfTwo3DigitNumbers100001(self):
+        self.assertEqual(problem4.isProductOfTwo3DigitNumbers(100001), False)
+
+    def test_isProductOfTwo3DigitNumbers899Squared(self):
+        self.assertEqual(problem4.isProductOfTwo3DigitNumbers(899*899), True)
+
+    def test_FinalSolution(self):
+        self.assertEqual(problem4.findLargestPalindromeProductOfTwo3DigitNumbers(999*999), 906609)
 
 if __name__ == '__main__':
     unittest.main()
