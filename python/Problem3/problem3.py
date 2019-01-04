711f23314e3f1eee533e9196ad5daf50f1f161dd
Chris McLean <christopher.mclean@gmail.com>
2019-01-04 18:11:34 +0800

    New isPrime function - Python Problem 3
    Updated python implimentation of problem 3 to make use of new shared isPrime function

--- python/Problem3/problem3.py
+++ python/Problem3/problem3.py
@@ -1,16 +1,12 @@
 #Solution to Project Euler problem 3 - https://projecteuler.net/problem=3
 #What is the largest prime factor of the number 600851475143.
 import math 
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
 
-def isPrime(num):
-  if num <= 1: return False
-  if num % 2 == 0: return False
-  upperLimit = math.floor(math.sqrt(num))
-  #we can increment i by 2 as every skipped value will be even and thus the %2 factor check above would have matched
-  for i in range(3,upperLimit+1, 2):
-      if num % i == 0:
-        return False
-  return True
+from shared.prime import isPrime
 
 def largestPrimeFactor(num):
   largestPrimeFactor = 0
@@ -27,5 +23,5 @@ def largestPrimeFactor(num):
           largestPrimeFactor = j
   return largestPrimeFactor
 
-result = largestPrimeFactor(600851475143)
-print(result)
+#result = largestPrimeFactor(600851475143)
+#print(result)
--- python/Problem3/test3.py
+++ python/Problem3/test3.py
@@ -3,42 +3,6 @@ import problem3
 
 class Test_TestProblem3(unittest.TestCase):
 
-    def test_isPrime1(self):
-        self.assertEqual(problem3.isPrime(1), False)
-
-    def test_isPrime2(self):
-        self.assertEqual(problem3.isPrime(2), False)
-
-    def test_isPrime5(self):
-        self.assertEqual(problem3.isPrime(5), True)
-    
-    def test_isPrime13(self):
-        self.assertEqual(problem3.isPrime(13), True)
-
-    def test_isPrime35(self):
-        self.assertEqual(problem3.isPrime(35), False)
-
-    def test_isPrime109(self):
-        self.assertEqual(problem3.isPrime(109), True)
-
-    def test_isPrime195(self):
-        self.assertEqual(problem3.isPrime(195), False)
-
-    def test_isPrime197(self):
-        self.assertEqual(problem3.isPrime(197), True)
-    
-    def test_isPrime199(self):
-        self.assertEqual(problem3.isPrime(199), True)
-
-    def test_isPrime200(self):
-        self.assertEqual(problem3.isPrime(200), False)
-
-    def test_isPrime6857(self):
-        self.assertEqual(problem3.isPrime(6857), True)
-
-    def test_isPrime6859(self):
-        self.assertEqual(problem3.isPrime(6859), False)
-
     def test_largestPrimeFactor13195(self):
         self.assertEqual(problem3.largestPrimeFactor(13195), 29)
 
