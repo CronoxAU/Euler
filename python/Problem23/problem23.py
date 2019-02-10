26c383fa7cda78ecfdd4f0818a47c73f39c0b63d
Chris McLean <christopher.mclean@gmail.com>
2019-02-10 14:23:41 +0800

    problem 23 update
    Began updating solution to problem 23 in python to adopt a more efficent solution

--- python/Problem23/problem23.py
+++ python/Problem23/problem23.py
@@ -1,6 +1,25 @@
 #Solution to Project Euler problem 23 - https://projecteuler.net/problem=23
 #Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
 
+#METHOD
+# create a list of all abundant numbers below the limit
+# Use this list to create a list of all the sums of abundant numbers below the limit
+# Calculate the sum of all numbers in the list, this is the inverse of the needed solution (ie the sum of all numbers which can be written as the sum of two abundant numbers)
+# Calculate the sum of all numbers below the limit
+# Subreact the first sum from the second to get the needed solution
+
+
+# Returns the sum of all the positive integers below the supplied limit which cannot be written as the sum of two abundant numbers.
+# As all numbers above 28123 can be written as the sum of an abundant number, any limit higher than this will return the same result
+def sumOfNonAbundantSums(limit):
+  if limit > 28123:
+    limit = 28123
+  abList = generateAbundantList(limit)
+  sumList = generateListOfSums(abList, limit)
+  sumOfAbundantSums = sumList(sumList)
+  triangleNum = triangleNumber(limit)
+  return triangleNum - sumOfAbundantSums
+
 #returns the sum of the factors of the supplied number n
 def sumOfFactors(n):
   sum = 0
@@ -21,31 +40,54 @@ def generateAbundantList(limit):
   abList = []
   for i in range(1,limit+1):
     if isAbundant(i):
-      abList.push(i)
+      abList.append(i)
   return abList
 
-#returns true is n is the sum of two digits in the supplied list
+#generates a deduplicated, sorted list of all possible numbers, below the supplied limit, created by adding two elements from the supplied list
+def generateListOfSums(list, limit):
+  result = []
+  for i in range (0,len(list)):
+    for j in range (0,len(list)):
+      if(list[i]+list[j]) <= limit:
+        result.append(list[i]+list[j])
+      else:
+        break
+  result = deduplicateList(result)
+  result.sort()
+  return result
+
+# deduplicates the provided list
+def deduplicateList(list):
+  result = []
+  for i in list:
+    if i not in result:
+      result.append(i)
+  return result
+
+# Returns the sum of all items in the list
+def sumList(list):
+  result = 0
+  for i in list:
+    result += i
+  return result
+
+# returns the n-th triangle number, ie the sum of all numbers up to and including n
+def triangleNumber(n):
+  return (n*n + n)//2
+
+  #returns true is n is the sum of two digits in the supplied list
+# NOTE - this function only works on a sorted list, this was implemented for optimization.
+# No longer used in current solution
 def isSumFromList(n,list):
-  list.sort()
+  #list.sort()
   for i in range (0,len(list)):
     for j in range (0,len(list)):
       if list[i] + list[j] == n:
         return True
-      j =+ 1
       if list[j] > n:
         break
-    i =+ 1
     if list[i] > n:
       break
+  # if no match was found return false
+  return False
 
-# Returns the sum of all the positive integers below the supplied limit which cannot be written as the sum of two abundant numbers.
-# As all numbers above 28123 can be written as the sum of an abundant number, any limit higher than this will return the same result
-def fullSolution(limit):
-  result = 0
-  if limit > 28123:
-    limit = 28123
-  abList = generateAbundantList(limit)
-  for i in range(1,limit+1):
-    if not isSumFromList(i,abList):
-      result += i
-  return result
--- python/Problem23/test23.py
+++ python/Problem23/test23.py
@@ -3,6 +3,7 @@ import problem23
 
 class Test_TestProblem21(unittest.TestCase):
 
+    # sumOfFactors function
     def test_sumOfFactors2(self):
         self.assertEqual(problem23.sumOfFactors(2), 1)
 
@@ -12,27 +13,64 @@ class Test_TestProblem21(unittest.TestCase):
     def test_sumOfFactors12(self):
         self.assertEqual(problem23.sumOfFactors(12), 16)
 
+
+    # isAbundant function
     def test_isAbundant4(self):
         self.assertEqual(problem23.isAbundant(4), False)
 
     def test_isAbundant12(self):
         self.assertEqual(problem23.isAbundant(12), True)
 
-    def test_isSumFromList1(self):
+    def test_isAbundant1(self):
         self.assertEqual(problem23.isSumFromList(1,[0,4,5,6]), False)
 
+
+    # isAbundant function
     def test_isSumFromList9(self):
         self.assertEqual(problem23.isSumFromList(9,[0,4,5,6]), True)  
 
+    # generateAbundantList function
     def test_generateAbundantList11(self):
         self.assertEqual(problem23.generateAbundantList(11), [])
 
     def test_generateAbundantList12(self):
         self.assertEqual(problem23.generateAbundantList(12), [12])
+
+    # generateListOfSums function
+    def test_generateListOfSums(self):
+        self.assertEqual(problem23.generateListOfSums([1,2,3],7), [2,3,4,5,6])
+
+    # Test the limit
+    def test_generateListOfSumsLimit(self):
+        self.assertEqual(problem23.generateListOfSums([1,2,3],3), [2,3])
+
+
+    # sumList function
+    def test_sumList(self):
+        self.assertEqual(problem23.sumList([1,2,3]), 6)
         
+    # triangleNumber function
+    def test_triangleNumber1(self):
+        self.assertEqual(problem23.triangleNumber(1), 1)
+
+    def test_triangleNumber3(self):
+        self.assertEqual(problem23.triangleNumber(3), 6)
+
+
+    # deduplicateList function
+    def test_deduplicateList1(self):
+        self.assertEqual(problem23.deduplicateList([1,1,1,1,1,2]), [1,2])
+
+    def test_deduplicateList2(self):
+        self.assertEqual(problem23.deduplicateList([1,2]), [1,2])
+
+    # sumOfNonAbundantSums function 
+    def test_sumOfNonAbundantSums11(self):
+        self.assertEqual(problem23.sumOfNonAbundantSums(11), 66)
+
     #test of full solution
-    def test_fullSolution(self):
-        self.assertEqual(problem23.fullSolution(28123), 0)
+    def test_sumOfNonAbundantSums28123(self):
+        self.assertEqual(problem23.sumOfNonAbundantSums(28123), 0)
 
 if __name__ == '__main__':
     unittest.main()
