540f904b138e51409ec685203dc081f6643020e3
Christopher McLean <christopher.mclean@gmail.com>
2019-02-15 16:25:00 +0800

    Problem 26 in Python
    Completed implimentation and testing for solution to problem 26 in Python

--- python/Problem26/problem26.py
+++ python/Problem26/problem26.py
@@ -1,18 +1,31 @@
 # Solution to Project Euler problem 26 - https://projecteuler.net/problem=26
 # Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
 
+# Notes:
+# See https://en.wikipedia.org/wiki/Repeating_decimal for samples and details
 
-def recurringLength(n):
-    p = 1
-    while (10 ** p) % n != 1:
-        p +=1
-    return p
 
-def maxRecurringLength(limit):
-    maxNumber = 0
-    maxValue = 0
+def recurringLengthOfUnitFraction(n):
+    remainderLog = []
+    remainder = 1
+    while True:
+        #raise the existing remainder by 10, as we are moving left one digit then calculate the new remainder
+        remainder = (remainder*10)%n
+        # if the remainder is 0 we have reached an even division, and there is no recurring section
+        if remainder == 0:
+            return 0
+        # if the current remainder already exists in our log we are ad the start of a new sequence, return the number of elements in the log since
+        # this number last appeared as the length of the recurring portion
+        if remainder in remainderLog:
+            return len(remainderLog) - remainderLog.index(remainder)
+        remainderLog.append(remainder)
+
+def maxUnitFractionRecurringLength(limit):
+    maxI = 0
+    maxLength = 0
     for i in range(1,limit):
-        currentValue = recurringDecimalLength(1,i)
-        if(currentValue > maxValue):
-            maxNumber = i
-    return maxNumber
+        currentLength = recurringLengthOfUnitFraction(i)
+        if(currentLength > maxLength):
+            maxI = i
+            maxLength = currentLength
+    return maxI
--- python/Problem26/test26.py
+++ python/Problem26/test26.py
@@ -2,23 +2,28 @@ import unittest
 import problem26
 
 class Test_TestProblem26(unittest.TestCase):
-    def test_recurringLength3(self):
-        self.assertEqual(problem26.recurringLength(3), 1)
+    def test_recurringLengthOfUnitFraction3(self):
+        self.assertEqual(problem26.recurringLengthOfUnitFraction(3), 1)
 
+    def test_recurringLengthOfUnitFraction7(self):
+        self.assertEqual(problem26.recurringLengthOfUnitFraction(7), 6)
 
-    def test_recurringLength28(self):
-        self.assertEqual(problem26.recurringLength(28), 6)
+    def test_recurringLengthOfUnitFraction9(self):
+        self.assertEqual(problem26.recurringLengthOfUnitFraction(9), 1)
 
-    def test_recurringLength43(self):
-        self.assertEqual(problem26.recurringLength(43), 21)
+    def test_recurringLengthOfUnitFraction28(self):
+        self.assertEqual(problem26.recurringLengthOfUnitFraction(28), 6)
+
+    def test_recurringLengthOfUnitFraction43(self):
+        self.assertEqual(problem26.recurringLengthOfUnitFraction(43), 21)
         
     
-    def test_maxRecurringLength10(self):
-        self.assertEqual(problem26.maxRecurringLength(10), 6)
+    def test_maxUnitFractionRecurringLength10(self):
+        self.assertEqual(problem26.maxUnitFractionRecurringLength(10), 7)
 
     #test of full solution
-    def test_maxRecurringLength100(self):
-        self.assertEqual(problem26.maxRecurringLength(100), 0)
+    def test_maxUnitFractionRecurringLength1000(self):
+        self.assertEqual(problem26.maxUnitFractionRecurringLength(1000), 983)
 
 if __name__ == '__main__':
     unittest.main()
