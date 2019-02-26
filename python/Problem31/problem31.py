fb47674692dd5e0a875c5e1bc35c6559387387db
Christopher McLean <christopher.mclean@gmail.com>
2019-02-26 12:19:42 +0800

    Solution for problem 31 in Python
    Implimented an solution for problem 31 in Python

--- python/Problem31/problem31.py
+++ python/Problem31/problem31.py
@@ -6,8 +6,11 @@ def getTotalFromCoins(nP2, nP1, np50, np20, np10, np5, np2, np1):
     return (nP2 *200) + (nP1 * 100) + (np50 * 50) + (np20 * 20) + (np10 * 10) + (np5 * 5) + (np2 * 2) + np1
 
 
-
 # returns the number of different ways to make n pounds, using standard English coins (1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p))
+# brute force approch, checking each combination of coins, breaking when the result is higher then the traget value
+# the only optimisation is assuming that once we reach the inner most for loop (the one for 1p) there will be a valid combination, where 1p coins is equal to the tagert value - the current test value
+# this value si not calculated, with one just being added to the count
+# this code is suprisingly quick but horrible to read and maintain. i would like to produce a cleaner version.
 def numberOfWaysToMakeNPounds(n):
     count = 0
     iP2 = 0 
@@ -17,33 +20,63 @@ def numberOfWaysToMakeNPounds(n):
     ip10 = 0
     ip5 = 0
     ip2 = 0
-    ip1 = 0
+    ip1 = 1
+    testValue = getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1)
     # work in pence it avoid issues with rounding
     nPence = int(n *100)
     # loop through each of the different options, counting up, breaking when we get to a result higher than the target value.
-    while getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1) <= nPence:
-        while getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1) <= nPence:
-            while getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1) <= nPence:
-                while getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1) <= nPence:
-                    while getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1) <= nPence:
-                        while getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1) <= nPence:
-                            while getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1) <= nPence:
-                                while getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1) <= nPence:
-                                    if getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1) == nPence:
-                                        count += 1
-                                    ip1 += 1
+    while testValue <= nPence:
+        while testValue <= nPence:
+            while testValue <= nPence:
+                while testValue <= nPence:
+                    while testValue <= nPence:
+                        while testValue <= nPence:
+                            while testValue <= nPence:
+                                if testValue <= nPence:
+                                    # when we reach the inner loop we can just add one pence at a time until we reach the nPence
+                                    # rather than calculating the number needed, just count this as another combination and move on
+                                    #if getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1) == nPence:
+                                    count += 1
                                 ip1 = 0
                                 ip2 += 1
+                                testValue = getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1)
+                                if testValue == nPence:
+                                    count += 1
+                                    break
                             ip2 = 0
                             ip5 += 1
+                            testValue = getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1)
+                            if testValue == nPence:
+                                count += 1
+                                break
                         ip5 = 0
                         ip10 += 1
+                        testValue = getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1)
+                        if testValue == nPence:
+                            count += 1
+                            break
                     ip10 = 0
                     ip20 += 1
+                    testValue = getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1)
+                    if testValue == nPence:
+                        count += 1
+                        break
                 ip20 = 0
                 ip50 += 1
+                testValue = getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1)
+                if testValue == nPence:
+                    count += 1
+                    break
             ip50 = 0
             iP1 += 1
+            testValue = getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1)
+            if testValue == nPence:
+                count += 1
+                break
         iP1 = 0
         iP2 += 1
+        testValue = getTotalFromCoins(iP2,iP1,ip50,ip20,ip10,ip5,ip2,ip1)
+        if testValue == nPence:
+            count += 1
+            break
     return count
--- python/Problem31/test31.py
+++ python/Problem31/test31.py
@@ -7,7 +7,7 @@ class Test_TestProblem31(unittest.TestCase):
         self.assertEqual(problem31.getTotalFromCoins(0,0,0,0,0,0,0,1), 1)
 
     def test_getTotalFromCoins2P(self):
-        self.assertEqual(problem31.getTotalFromCoins(2,0,0,0,0,0,0,0), 200)
+        self.assertEqual(problem31.getTotalFromCoins(2,0,0,0,0,0,0,0), 400)
 
     def test_getTotalFromCoinsOneOfEach(self):
         self.assertEqual(problem31.getTotalFromCoins(1,1,1,1,1,1,1,1), 388)
@@ -22,11 +22,11 @@ class Test_TestProblem31(unittest.TestCase):
         self.assertEqual(problem31.numberOfWaysToMakeNPounds(.05), 4)
 
     def test_numberOfWaysToMakeNPounds6p(self):
-        self.assertEqual(problem31.numberOfWaysToMakeNPounds(.06), 6)
+        self.assertEqual(problem31.numberOfWaysToMakeNPounds(.06), 5)
 
     #test of full solution
-    #def test_sumOfAllMthPower(self):
-    #    self.assertEqual(problem31.numberOfWaysToMakeNPounds(2), 0)
+    def test_sumOfAllMthPower(self):
+        self.assertEqual(problem31.numberOfWaysToMakeNPounds(2), 73682)
 
 if __name__ == '__main__':
     unittest.main()
